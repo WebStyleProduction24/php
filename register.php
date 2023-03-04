0220c95399f071a70830ff3e5b283b88188bb0c6
Jack36VRN <36456429+Jack36VRN@users.noreply.github.com>
2023-03-05 00:34:17 +0300

    Update 05/03/2023 0:23 +3GMT (MSK)
    1. head->header.php;
    2. Удалено сообщение о том, что сеанс уже активен и session_start() игнорируется

--- register.php
+++ register.php
@@ -1,21 +1,4 @@
-<!DOCTYPE html>
-<html lang="en">
-	<head>
-		<meta charset="UTF-8">
-		<meta name="viewport" content="width=device-width, initial-scale=1.0">
-		<title>Регистрация</title>
-
-		<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">		
-		<link rel="stylesheet" href="style.css">
-
-		<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js" integrity="sha384-w76AqPfDkMBDXo30jS1Sgez6pr3x5MlQ1ZAGC+nuZB+EYdgRZgiwxhTBTkF7CXvN" crossorigin="anonymous"></script>
-		<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.3/jquery.min.js"></script>
-		<script>
-			$(function(){
-				$(".alert").delay(3000).slideUp(300);
-			});
-		</script>
-	</head>
+<?php include 'header.php';?>
 
 	<body>
 		<form class="p-5 mb-5 text-bg-light border rounded" method="post" action="" name="signin-form">
